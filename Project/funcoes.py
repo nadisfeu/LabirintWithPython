603e330909ca22893f776b84797cac87d48c285d
nandisfeu <fernando.figueredo@aluno.ufop.edu.br>
2023-01-27 23:02:55 -0300

    adicionando comentarios e função pra fechar

--- Project/funcoes.py
+++ Project/funcoes.py
@@ -2,10 +2,11 @@
 class Funcoes:
 
     def __init__(self, file_name=str):
-        self.file = open(file_name, 'r')
-        self.file_content = self.file.read()
+        self.file = open(file_name, 'r')             # Abre o txt
+        self.file_content = self.file.read()         # Devolve o conteudo do txt
         self.lines = self.file_content.splitlines()  # Transforma cada linha em um elemento na lista
-        self.num_lines = len(self.lines)
+        self.num_lines = len(self.lines)             # Devolve o numero de linhas
+        self.node = self._node_count()               # Devolve o numero de vertices
 
     def _node_count(self) -> int:
         count = 0
@@ -15,3 +16,6 @@ class Funcoes:
 
     def print_file(self):
         print(self.file_content)
+
+    def _close_file(self):
+        self.file.close()
