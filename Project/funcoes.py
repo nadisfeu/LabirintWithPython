7261517fe62c0d30a03206274e45243a427a92b4
Graziele-Rodrigues <c.graziele.rodrigues@gmail.com>
2023-02-05 20:14:12 -0300

    funcao busca dentro do arquivo funcoes.py

--- Project/funcoes.py
+++ Project/funcoes.py
@@ -55,9 +55,22 @@ class Funcoes:
                 node += 1
 
 
-
-
-
-
-
-                    
+def busca(graph, start, end, visited=None):
+    if visited is None:
+        visited = set()
+    # Marca o vértice atual como visitado
+    visited.add(start)
+    # Verifica se o vértice atual é o destino
+    if start == end:
+        return [start]
+    # Para cada vizinho do vértice atual
+    for neighbor in graph.get_neighbors(start):
+        # Se o vizinho ainda não foi visitado
+        if neighbor not in visited:
+            # Realiza a busca em profundidade a partir do vizinho
+            path = busca(graph, neighbor, end, visited)
+            # Se a busca retornou um caminho válido, adiciona o vértice atual ao caminho
+            if path is not None:
+                return [start] + path
+    # Se não há um caminho válido a partir do vértice atual, retorna None
+    return None
--- Project/main.py
+++ Project/main.py
@@ -1,28 +1,7 @@
-from funcoes import Funcoes
+from funcoes import *
 
 
-def busca(graph, start, end, visited=None):
-    if visited is None:
-        visited = set()
-    # Marca o vértice atual como visitado
-    visited.add(start)
-    # Verifica se o vértice atual é o destino
-    if start == end:
-        return [start]
-    # Para cada vizinho do vértice atual
-    for neighbor in f.graph.get_neighbors(start):
-        # Se o vizinho ainda não foi visitado
-        if neighbor not in visited:
-            # Realiza a busca em profundidade a partir do vizinho
-            path = busca(graph, neighbor, end, visited)
-            # Se a busca retornou um caminho válido, adiciona o vértice atual ao caminho
-            if path is not None:
-                return [start] + path
-    # Se não há um caminho válido a partir do vértice atual, retorna None
-    return None
-
-
-file_path =  'C:/Users/grazi/OneDrive/Área de Trabalho/Arquivos/LabirintWithPython/Anexo/maze/toy.txt'
+file_path =  'C:/Users/grazi/OneDrive/Área de Trabalho/Arquivos/LabirintWithPython/Anexo/maze/maze3.txt'
 f = Funcoes(file_path)
 f.print_file()
 print("**********************")
